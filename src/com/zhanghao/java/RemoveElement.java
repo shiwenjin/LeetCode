d0a532821334976f853bcb0e921d5256e4fc7cb9
shiwenjin <zh376981735@gmail.com>
2017-08-21 23:31:55 +0800

    根据给定的值，删除数组中的元素

---
+++ src/com/zhanghao/java/RemoveElement.java
@@ -0,0 +1,46 @@
+package com.zhanghao.java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Created by zhangh on 2017/8/21.
+ */
+public class RemoveElement {
+    public static void main(String[] args) {
+        int[] nums = {3,2,2,3};
+        int val = 2;
+        System.out.println(removeElement(nums, val));
+    }
+
+    private static int removeElement(int[] nums, int val) {
+        Integer[] a =  new Integer[nums.length];
+        for (int i = 0; i < nums.length; i++)
+            a[i] = Integer.valueOf(nums[i]);
+
+
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(a));
+
+        Iterator<Integer> sListIterator = arrayList.iterator();
+
+        while(sListIterator.hasNext()) {
+            Integer i = sListIterator.next();
+            if(i.equals(val))
+                sListIterator.remove();
+         }
+
+         return arrayList.size();
+
+//        正确答案：
+//        int num = 0;
+//        for (int i = 0; i< nums.length; i++)
+//        {
+//            if (nums[i] != val)
+//                nums[num++] = nums[i];
+//        }
+//        return num;
+    }
+
+
+}
