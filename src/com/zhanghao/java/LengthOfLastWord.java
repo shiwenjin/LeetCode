ebcb0e4ae3b6307ce590f578d370d7bd4dbae621
shiwenjin <zh376981735@gmail.com>
2017-08-30 23:06:18 +0800

    修复错误

--- src/com/zhanghao/java/LengthOfLastWord.java
+++ src/com/zhanghao/java/LengthOfLastWord.java
@@ -5,7 +5,8 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Created by zhangh on 2017/8/30 20:45.
- *
+ * 最后一个单词的长度
+ * 判断非空应该先写在前面
  */
 public class LengthOfLastWord {
     private static Logger logger = LoggerFactory.getLogger(PalindromeNumber.class);
@@ -15,12 +16,12 @@ public class LengthOfLastWord {
     }
 
     private static int lengthOfLastWord(String s) {
-        if (s.length() == 0 || s.trim().length() == 0 || s == null) {
+        if (s == null || s.length() == 0 || s.trim().length() == 0 ) {
             return 0;
         } else {
             String[] listStr = s.split(" ");
-            logger.error("最后一个字符串为：{}", listStr);
-            return listStr[listStr.length - 1].length();
+                logger.error("最后一个字符串为：{}", listStr[listStr.length - 1]);
+                return listStr[listStr.length - 1].length();
+            }
         }
     }
-}
